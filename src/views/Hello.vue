1e7c3e7d324cbebd006f86fa06fedcb826723b60
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2020-05-16 20:38:29 +0900

    Initial Commit

---
+++ src/views/Hello.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>ハローワールドですね</h1>
+    <p>Body: {{ body }}</p>
+    <p>Datetime: {{ datetime }}</p>
+    <button @click="getHello">更新するよ</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'hello',
+  data() {
+    return {
+      body: '',
+      datetime: null,
+    };
+  },
+  methods: {
+    async getHello() {
+      const response = await axios.get('/api');
+      this.body = response.data.body;
+      this.datetime = response.data.datetime;
+    },
+  },
+  created() {
+    axios.defaults.baseURL = process.env.VUE_APP_API_BASE_URL;
+  },
+  mounted() {
+    this.getHello();
+  },
+};
+</script>
